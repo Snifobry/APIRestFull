5686fc8d8065a8575f8250107f7fc5285a17abf5
Snifobry <brycardsnifo@gmail.com>
2024-08-06 15:47:06 -0500

    Se agrego un @get por id en el controllador  estamos haciendo el CRUD

--- src/main/java/com/spring_app/RestController/ProductoController.java
+++ src/main/java/com/spring_app/RestController/ProductoController.java
@@ -2,6 +2,7 @@ package com.spring_app.RestController;
 
 import com.spring_app.Entidad.Producto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,10 +20,20 @@ public class ProductoController {
         productos.add(new Producto(3,"Gogeta", 1,25.99));
     }
 
-    @GetMapping("/productos")
+    @GetMapping("/api/productos")
     public List<Producto> listarProductos() {
         return productos;
     }
 
-
+    @GetMapping("/api/productos/{id}")
+    //@Pathvariable hace q la ide de ruta /{id} se injecte en el controllador para buscar por id
+    //despues del path donde dice "int id" esto no debe cambiar asi esta hecho no cambies Snifo xfa
+    public Producto buscarProducto(@PathVariable int id) {
+        for (Producto producto: productos) {
+            if(producto.getIdProducto()==id){
+                return producto;
+            }
+        }
+        return null;
+    }
 }
